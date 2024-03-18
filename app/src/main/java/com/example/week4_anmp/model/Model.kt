d35bb35b6106360d7897ad269f6d15e3b619dc08
Andre Andre <>
2024-03-18 10:04:14 +0700

    init

---
+++ app/src/main/java/com/example/week4_anmp/model/Model.kt
@@ -0,0 +1,11 @@
+package com.example.week4_anmp.model
+
+
+
+data class Student(
+    var id:String?,
+    var name:String?,
+    var dob:String?,
+    var phone:String?,
+    var photoUrl:String?
+)
---
+++ app/src/main/java/com/example/week4_anmp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.week4_anmp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.week4_anmp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
